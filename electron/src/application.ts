b14811e14539306b0894cfe86174a663c892f543
0x26B <0x26b@unknown.com>
2023-12-30 17:35:51 -0600

    Inject root path, cahce get all modules, get control / identifier / category / module logic.

--- electron/src/application.ts
+++ electron/src/application.ts
@@ -1,18 +1,116 @@
 import { FileInteractor } from './filesystem-file-interactor';
 import { JsonParser } from './filesystem-json-parser';
+import { IModule } from './models/dcs-module';
 
 export class Application {
+    private _root: string;
     private _fileInteractor: FileInteractor;
     private _jsonParser: JsonParser;
+    private _modules: IModule[] = [];
 
-    constructor(fileInteractor: FileInteractor, jsonParser: JsonParser) {
+    constructor(root: string, fileInteractor: FileInteractor, jsonParser: JsonParser) {
+        this._root = root;
         this._fileInteractor = fileInteractor;
         this._jsonParser = jsonParser;
     }
 
-    getAllModules = async (root: string) => {
-        const rawModules = await this._fileInteractor.readAllModuleJsonFiles(root);
+    setRootPath = async (root: string) => {
+        this._root = root;
+    };
+
+    updateModulesFromDisk = async () => {
+        const rawModules = await this._fileInteractor.readAllModuleJsonFiles(this._root);
         const deserializedModules = await this._jsonParser.deserializeAllModules(rawModules);
-        return deserializedModules;
+        this._modules = deserializedModules;
+    };
+
+    getAllModules = async () => {
+        return this._modules;
+    };
+
+    getModuleByName = async (moduleName: string) => {
+        const module = this._modules.find(module => {
+            return module.name === moduleName;
+        });
+
+        if (!module) {
+            throw new Error(`Module name: ${moduleName} not found`);
+        }
+
+        return module;
+    };
+
+    getAllModuleNames = async () => {
+        const moduleNames = [];
+        for (const module of this._modules) {
+            moduleNames.push(module.name);
+        }
+        return moduleNames;
+    };
+
+    getAllCategoryNamesByModuleName = async (moduleName: string) => {
+        const module = this._modules.find(module => {
+            return module.name === moduleName;
+        });
+
+        if (!module) {
+            throw new Error(`Module name: ${moduleName} not found`);
+        }
+
+        const categoryNames = [];
+        for (const category of module.documentation.categories) {
+            categoryNames.push(category.name);
+        }
+
+        return categoryNames;
+    };
+
+    getAllControlIdentifiersByNames = async (moduleName: string, categoryName: string) => {
+        const module = await this.getModuleByName(moduleName);
+
+        if (!module) {
+            throw new Error(`Module name: ${moduleName} not found`);
+        }
+
+        const category = module.documentation.categories.find(category => {
+            return category.name === categoryName;
+        });
+
+        if (!category) {
+            throw new Error(`Category name: ${categoryName} not found`);
+        }
+
+        const controlIdentifiers = [];
+        for (const control of category.controls) {
+            controlIdentifiers.push(control.identifier);
+        }
+
+        return controlIdentifiers;
+    };
+
+    getControlByNames = async (moduleName: string, categoryName: string, identiferName: string) => {
+        const module = await this.getModuleByName(moduleName);
+
+        if (!module) {
+            throw new Error(`Module name: ${moduleName} not found`);
+        }
+
+        const category = module.documentation.categories.find(category => {
+            return category.name === categoryName;
+        });
+
+        if (!category) {
+            throw new Error(`Category name: ${categoryName} not found`);
+        }
+
+        const control = category.controls.find(control => {
+            return control.identifier === identiferName;
+        });
+
+        if (!control) {
+            throw new Error(`Control identifier: ${identiferName} not found`);
+        }
+
+        return control;
     };
 }
