6b74557cbc175fabf3c921237e2829937c545c36
0x26B <0x26b@unknown.com>
2023-12-30 17:36:19 -0600

    Update input/output types, and make last few optional.

--- electron/src/models/dcs-control.ts
+++ electron/src/models/dcs-control.ts
@@ -2,20 +2,26 @@
 // https://github.com/DCS-Skunkworks/dcs-bios/blob/master/Scripts/DCS-BIOS/lib/modules/documentation/Control.lua
 
 import { ControlType } from './dcs-control-type';
-import { IInput } from './dcs-input';
-import { IOutput } from './dcs-output';
 import { MomentaryPositions } from './dcs-momentary-positions';
 import { PhysicalVariant } from './dcs-physical-variant';
 import { ApiVariant } from './dcs-api-variant';
+import { IInput } from './dcs-input';
+import { IActionInput } from './dcs-action-input';
+import { IFixedStepInput } from './dcs-fixed-step-input';
+import { ISetStateInput } from './dcs-set-state-input';
+import { ISetStringInput } from './dcs-set-string-input';
+import { IOutput } from './dcs-output';
+import { IStringOutput } from './dcs-string-output';
+import { IIntegerOutput } from './dcs-integer-output';
 
 export type IControl = {
     category: string;
     controlType: ControlType;
     identifier: string;
     description: string;
-    inputs: IInput[];
-    outputs: IOutput[];
-    momentaryPositions: MomentaryPositions;
-    physicalVariant: PhysicalVariant;
-    apiVariant: ApiVariant;
+    inputs: (IActionInput | IFixedStepInput | ISetStateInput | ISetStringInput)[];
+    outputs: (IIntegerOutput | IStringOutput)[];
+    momentaryPositions?: MomentaryPositions;
+    physicalVariant?: PhysicalVariant;
+    apiVariant?: ApiVariant;
 };
