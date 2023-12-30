43d011a856b774f0824a6fe73b09a453c7bf83b0
0x26B <0x26b@unknown.com>
2023-12-30 17:28:07 -0600

    Updating the variable step input to inherit from the base input.

--- electron/src/models/dcs-variable-step-input.ts
+++ electron/src/models/dcs-variable-step-input.ts
@@ -1,11 +1,9 @@
 // See:
 // https://github.com/DCS-Skunkworks/dcs-bios/blob/master/Scripts/DCS-BIOS/lib/modules/documentation/VariableStepInput.lua
 
-import { InputType } from './dcs-input-type';
+import { IInput } from './dcs-input';
 
-export type IVariableStepInput = {
-    interface: InputType.variableStep;
+export interface IVariableStepInput extends IInput {
     suggestedStep: number;
     maxValue: number;
-    description: string;
-};
+}
