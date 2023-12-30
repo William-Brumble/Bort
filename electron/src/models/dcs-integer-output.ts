a71baba2a6ce80e25cc3c233b791a8a5a3cbcc5f
0x26B <0x26b@unknown.com>
2023-12-30 17:29:17 -0600

    Updating the integer output to inherit from the base output.

--- electron/src/models/dcs-integer-output.ts
+++ electron/src/models/dcs-integer-output.ts
@@ -1,15 +1,10 @@
 // See:
 // https://github.com/DCS-Skunkworks/dcs-bios/blob/master/Scripts/DCS-BIOS/lib/modules/documentation/IntegerOutput.lua
 
-import { Suffix } from './dcs-suffix';
-import { OutputType } from './dcs-output-type';
+import { IOutput } from './dcs-output';
 
-export type IIntegerOutput = {
+export interface IIntegerOutput extends IOutput {
     mask: number;
     maxValue: number;
     shiftBy: number;
-    address: number;
-    suffix: Suffix;
-    description: string;
-    type: OutputType.integer;
-};
+}
